524807d20a84062497c7b6d6d22cc9b86954a8be
nathanaresler <nathan.a.resler@gmail.com>
2025-12-28 20:01:42 -0800

    Finished my first Project! yay!

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-    #
+        
     #print(character_count("sys.argv[1]"))
     print(f"""============ BOOKBOT ============
 Analyzing book found at {sys.argv[1]}...
